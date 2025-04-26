d3a50f0fc96afae80c87e9b601dbcb45116114f3
yogowang <wangyuci71@gmail.com>
2025-04-25 22:17:45 -0300

    add poll program

--- anchor/programs/anchor-program/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-declare_id!("HrnLkCivatsDhzp7PmLWRdPasPk4o6upf5WgvAoFyGCV");
-
-#[program]
-pub mod anchor_program {
-    use super::*;
-}
---
+++ anchor/programs/voting/src/lib.rs
@@ -0,0 +1,36 @@
+#![allow(clippy::result_large_err)]
+use anchor_lang::prelude::*;
+
+declare_id!("HrnLkCivatsDhzp7PmLWRdPasPk4o6upf5WgvAoFyGCV");
+
+#[program]
+pub mod anchor_program {
+    use super::*;
+    pub fn initialize_poll(ctx:Context<InitializePoll>,poll_id: u64) -> Result<()>{
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info>{
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    #[account(init,
+    payer=signer,
+    space=8+Poll::INIT_SPACE,
+    seeds=[poll_id.to_le_bytes().as_ref()],
+    bump,
+    )]
+    pub poll:Account<'info,Poll>,
+    pub system_program: Program<'info,System>,
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Poll{
+    pub poll_id: u64,
+    #[max_len(280)]
+    pub description: String,
+    pub poll_end: u64,
+    pub candidate_amount: u64,
+}
