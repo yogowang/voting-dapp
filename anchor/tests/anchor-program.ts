b4c1b0981732cd5506443eb4618d33326a0d6d90
yogowang <wangyuci71@gmail.com>
2025-05-02 19:53:35 -0300

    add first anchor test

--- anchor/tests/anchor-program.ts
+++ anchor/tests/anchor-program.ts
@@ -2,13 +2,24 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { BankrunProvider, startAnchor } from "anchor-bankrun";
 import { AnchorProgram } from "../target/types/anchor_program";
+import { PublicKey } from "@solana/web3.js";
 const IDL = require("../target/idl/anchor_program.json");
-
+const votingAddress= new PublicKey("HrnLkCivatsDhzp7PmLWRdPasPk4o6upf5WgvAoFyGCV");
 describe("voting", () => {
 
   it("voting initialized!", async () => {
-    const context = await startAnchor("", [], []);
+    const context = await startAnchor("", [{name:"voting",programId: votingAddress}], []);
 
 	  const provider = new BankrunProvider(context);
+    const votingProgram = new Program<AnchorProgram>(
+      IDL,
+      provider,
+    );
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "test poll",
+      new anchor.BN(0),
+      new anchor.BN(1746226096),
+    ).rpc();
   });
 });
